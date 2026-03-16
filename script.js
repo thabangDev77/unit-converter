940a566bd52d0d1d24f2af77cca704c97a7179bd
thabangDev77 <phoshokothabang20@gmail.com>
2026-03-16 20:37:01 +0200

    feat: add a function to calculate conversion logic and display the results

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 */
 
 //input section
-const inputValue = document.getElementById("inputValue");
+const inputValue = document.getElementById("inputValue")
 const convertBtn = document.getElementById("convertBtn")
 
 //results section
@@ -18,6 +18,20 @@ const volumeContainer = document.getElementById("volumeResult")
 const massContainer = document.getElementById("massResult")
 
 convertBtn.addEventListener("click", function() {
-    console.log("clicked")
-    console.log(inputValue.value * 3)
+    convertResults()
 })
+
+function convertResults() {
+    lengthContainer.textContent = `
+        ${inputValue.value} meters = ${(inputValue.value * 3.281).toFixed(3)} feet | 
+        ${inputValue.value} feet = ${(inputValue.value * 0.3048).toFixed(3)} meters
+    `
+    volumeContainer.textContent = `
+        ${inputValue.value} liters = ${(inputValue.value * 0.264).toFixed(3)} gallons | 
+        ${inputValue.value} gallons = ${(inputValue.value * 3.785).toFixed(3)} liters
+    `
+    massContainer.textContent = `
+        ${inputValue.value} kilograms = ${(inputValue.value * 2.204).toFixed(3)} pounds | 
+        ${inputValue.value} pounds = ${(inputValue.value * 0.453).toFixed(3)} kilograms
+    `
+}
